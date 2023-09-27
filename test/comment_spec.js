d96e5c988cc8f7e362144d947e2bb1375e63a385
Fadoli <f13@laposte.net>
2023-09-27 23:22:25 +0200

    un-bunify

--- test/comment_spec.js
+++ test/comment_spec.js
@@ -1,12 +1,25 @@
-import { expect, test, describe, beforeAll, beforeEach, afterAll, afterEach } from "bun:test";
+// import { expect, test, describe, beforeAll, beforeEach, afterAll, afterEach } from "bun:test";
+let { describe, test, expect, beforeEach, afterEach, after: afterAll, before: beforeAll } = require("node:test");
+
+// node wrapper
+if (!expect) {
+    const assert = require("node:assert");
+    expect = (input) => {
+        return {
+            toBe: (something) => {
+                assert.deepStrictEqual(input,something);
+            }
+        }
+    }
+}
 
 const helper = require("../index.js");
 const commentNode = require("./nodes/90-comment.js");
 
-describe('comment Node', async function () {
-    beforeAll((done) => helper.startServer(done));
-    afterAll((done) => helper.stopServer(done));
-    afterEach(() => helper.unload());
+describe('comment Node', function () {
+    beforeAll(() => helper.startServer());
+    afterAll(() => helper.stopServer());
+    // afterEach(() => helper.unload());
 
     test('should be loaded async', async function () {
         var flow = [{ id: "n1", type: "comment", name: "comment" }];
@@ -14,21 +27,8 @@ describe('comment Node', async function () {
         var n1 = helper.getNode("n1");
         expect(n1?.name).toBe('comment');
     });
-
-    test('should be loaded cb', function (done) {
-        var flow = [{ id: "n1", type: "comment", name: "comment" }];
-        helper.load(commentNode, flow, () => {
-            try {
-                var n1 = helper.getNode("n1");
-                expect(n1?.name).toBe('comment');
-                done();
-            } catch (err) {
-                done(err);
-            }
-        });
-    });
-
-    test('should be loaded async', async function () {
+    
+    test('should be loaded async and is in the runtime', async function () {
         var flow = [{ id: "n1", type: "comment", name: "comment" }];
         await helper.load(commentNode, flow);
         var n1 = helper.getNode("n1");
